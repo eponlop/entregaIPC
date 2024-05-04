6e90b7c1f1e0669f67e672cd1b3b7db3cf4d7ef7
eponlop <estebanplromeral@gmail.com>
2024-05-04 13:07:34 +0200

    commit de prueba

---
+++ src/controlador/Prueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author esteb
+ */
+public class Prueba {
+    private String name;
+
+    public Prueba(String name) {
+        this.name = name;
+    }
+    
+}
