b7f7459bbaa5f5e2ce5f095d46e1eb5a2957219c
Multimotivation <jomartiorts@gmail.com>
2024-05-11 13:40:39 +0200

    Modificaciones contenedor de añadir gasto

--- src/controlador/ContenedorPrincipalController.java
+++ src/controlador/ContenedorPrincipalController.java
@@ -14,17 +14,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
  *
  * @author esteb
- */
+ */ 
 public class ContenedorPrincipalController implements Initializable {
 
     @FXML
     private BorderPane borderPanePrincipal;
-
+    @FXML
     /**
      * Initializes the controller class.
      */
