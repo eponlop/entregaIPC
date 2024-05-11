462331fc0c11dfd54b4bf5d496a522caf4d1407f
eponlop <estebanplromeral@gmail.com>
2024-05-11 12:00:09 +0200

    Añadir gasto

--- src/controlador/ContenedorPrincipalController.java
+++ src/controlador/ContenedorPrincipalController.java
@@ -6,7 +6,11 @@ package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -15,12 +19,16 @@ import javafx.fxml.Initializable;
  */
 public class ContenedorPrincipalController implements Initializable {
 
+    @FXML
+    private BorderPane borderPanePrincipal;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        // borderPanePrincipal.setCenter("vista/AñadirGasto.fxml");
     }    
     
 }
