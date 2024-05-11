b89a4582125ccc0038a1c8d8930a5fbe6ef8368d
eponlop <estebanplromeral@gmail.com>
2024-05-11 12:21:06 +0200

    Añadido el fxml de añadir gasto

--- src/controlador/ContenedorPrincipalController.java
+++ src/controlador/ContenedorPrincipalController.java
@@ -14,8 +14,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 /**
  * FXML Controller class
