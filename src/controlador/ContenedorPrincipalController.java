e5017639af71554c395e3a32cf3c026be1b02bd0
ibane <ibane@Clara_I>
2024-05-19 18:38:37 +0200

    He creado el archivo ConfiguraciónPerfil.fxml y he modificado el controlador del contenedor principal para que se redirija a la ventana

--- src/controlador/ContenedorPrincipalController.java
+++ src/controlador/ContenedorPrincipalController.java
@@ -67,7 +67,7 @@ public class ContenedorPrincipalController implements Initializable {
     private void configuracion(MouseEvent event) {
         try {
             // cambia a la opción de configuración
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/RegistroUsuario.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/ConfiguraciónPerfil.fxml"));
             Parent configuracion = loader.load();
             borderPanePrincipal.setCenter(configuracion);
         } catch (IOException ex) {
